49e850be66f775ae141fc102d7cf6966e608b525
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-07-10 17:19:21 +0900

    complete chapter 3

--- src/main/kotlin/tdd/kotlin/Dollar.kt
+++ src/main/kotlin/tdd/kotlin/Dollar.kt
@@ -4,7 +4,7 @@ class Dollar(
     var amount: Int
 ) {
 
-    fun times(multiplier: Int): Dollar {
-        return Dollar(amount * multiplier)
-    }
+    fun times(multiplier: Int) = Dollar(amount * multiplier)
+
+    override fun equals(other: Any?) = (other as Dollar?)?.amount == amount
 }
--- src/test/kotlin/tdd/kotlin/DollarTest.kt
+++ src/test/kotlin/tdd/kotlin/DollarTest.kt
@@ -4,11 +4,16 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class DollarTest : StringSpec({
-    "tstMultiplication" {
+    "testMultiplication" {
         val five = Dollar(5)
         var product = five.times(2)
         product.amount shouldBe 10
         product = five.times(3)
         product.amount shouldBe 15
+    }
+
+    "testEquality" {
+        (Dollar(5) == Dollar(5)) shouldBe true
+        (Dollar(5) == Dollar(6)) shouldBe false
     }
 })
